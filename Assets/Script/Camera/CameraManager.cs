f4edb2b1b360fa470e7d3bf1e718e69d0f2e9d95
DESKTOP-FPQI6KL\user <cocapepsi123@daum.net>
2021-12-22 12:48:49 +0900

    에임점 건드는중

--- Assets/Script/Camera/CameraManager.cs
+++ Assets/Script/Camera/CameraManager.cs
@@ -8,12 +8,18 @@ public class CameraManager : Singleton<CameraManager>
 	protected BulletManager M_Bullet => BulletManager.Instance;
 	
 	bool movecheck;
+	Vector2 arrival;
 	public Vector3 GetPos()
 	{
 		Vector3 temp = M_Player.GetPos();
 		return temp;
 	}
-	
+	public Vector3 Arrival
+    {
+		get { return arrival; }
+        set { arrival = value; }
+    }
+	//public Vector3 Shot
 	public bool Movecheck
 	{
 		get { return movecheck; }
--- Assets/Script/Player/PlayerManager.cs
+++ Assets/Script/Player/PlayerManager.cs
@@ -6,38 +6,42 @@ public class PlayerManager : Singleton<PlayerManager>
 {
     Player M_Playerobj => Player.Instance;
     BulletManager M_Bullet => BulletManager.Instance;
-   
-    public void ShotBullet(int guntype,int count)
-	{
-        Bullet item=M_Bullet.Spawn(guntype, count);
-        item.transform.position= M_Playerobj.NozzleRotation.transform.position;
-        item.transform.localEulerAngles = new Vector3(M_Playerobj.NozzleRotation.transform.eulerAngles.x+90, M_Playerobj.transform.eulerAngles.y,0f);
+    _GameManager M_GAME => _GameManager.Instance;
+    CameraManager M_Camera => CameraManager.Instance;
+    public void ShotBullet(int guntype, int count)
+    {
+        Bullet item = M_Bullet.Spawn(guntype, count);
+       
+       
+        item.transform.position = M_Playerobj.NozzleRotation.transform.position;
+        item.transform.localEulerAngles = new Vector3(M_Playerobj.NozzleRotation.transform.eulerAngles.x + 90, M_Playerobj.transform.eulerAngles.y, 0f);
+      
         item.SetDir = item.transform.up;
     }
-   
+
     public bool ActiveSelf()
-	{
+    {
         return M_Playerobj.ActiveSelf;
-	}
+    }
     public void SetActivePlayer(bool active)
-	{
+    {
         M_Playerobj.gameObject.SetActive(active);
-	}
-    public void SetPos(float x, float y,float z)
-	{
-        M_Playerobj.SetPlayerPos(x,y,z);
-	}
+    }
+    public void SetPos(float x, float y, float z)
+    {
+        M_Playerobj.SetPlayerPos(x, y, z);
+    }
     public Vector3 GetPos()
-	{
+    {
         return M_Playerobj.GetPlayerPos;
-	}
+    }
     public GameObject GetObjPlayer
     {
         get { return M_Playerobj.gameObject; }
     }
     public void SetNozzleRotation(Vector3 angle)
-	{
-        M_Playerobj.NozzleRotation.transform.eulerAngles =angle;
-	}
-   
+    {
+        M_Playerobj.NozzleRotation.transform.eulerAngles = angle;
+    }
+
 }
